133a8d7506b5cbabd80a75efabae79af169db3d5
Kai Breese <kai.breeze@me.com>
2026-05-28 17:29:20 -0700

    Initial Commit

---
+++ SlipStream/SlipStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlipStreamApp.swift
+//  SlipStream
+//
+//  Created by Kai Breese on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SlipStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlipStreamTests/SlipStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlipStreamTests.swift
+//  SlipStreamTests
+//
+//  Created by Kai Breese on 5/28/26.
+//
+
+import Testing
+@testable import SlipStream
+
+struct SlipStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
